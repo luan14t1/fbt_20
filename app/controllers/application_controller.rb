e2e20330b7f4bcb617caa3c6f45d099d69e930cb
luan14t1e <you@example.com>
2018-02-06 13:44:03 +0700

    Admin Layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  include SessionsHelper
-
-  protect_from_forgery
-
   before_action :set_locale
+  protect_from_forgery with: :exception
 
   private
 
